aa1caab33c16369a9f87c779a06d65489f06acb3
sihun <thrchtlgns@naver.com>
2023-05-24 11:52:11 +0900

    feat 위젯 중앙배치

--- lib/main.dart
+++ lib/main.dart
@@ -17,7 +17,7 @@ class MyApp extends StatelessWidget {
         title: 'Namer App',
         theme: ThemeData(
           useMaterial3: true,
-          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepOrange),
+          colorScheme: ColorScheme.fromSeed(seedColor: Color.fromARGB(255, 52, 6, 177)),
         ),
         home: SafeArea(child : MyHomePage()),
       ),
@@ -27,28 +27,65 @@ class MyApp extends StatelessWidget {
 
 class MyAppState extends ChangeNotifier {
   var current = WordPair.random();
+
+  void getNext(){
+    current = WordPair.random();
+    notifyListeners();
+  }
 }
 
 class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     var appState = context.watch<MyAppState>();
-
+    var pair = appState.current;
     return Scaffold(
-      body: Column(
-        children: [
-
-          Text('A random idea123:'),
-          Text(appState.current.asLowerCase),
-          Text('hello world!'),
-          ElevatedButton(
-            onPressed: () {
-              print('button pressed!');
-            }, 
-            child: Text('Next'),
-          ),
-        ],
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+      
+            Text('A random idea123:'),
+            BigCard(pair: pair,),
+      
+            ElevatedButton(
+              onPressed: () {
+                print('button pressed!');
+                appState.getNext();
+              }, 
+              child: Text('Next'),
+            ),
+          ],
+        ),
       ),
     );
   }
+}
+
+class BigCard extends StatelessWidget {
+  const BigCard({
+    super.key,
+    required this.pair,
+  });
+  
+  final WordPair pair;
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);       // ← Add this.
+    final style = theme.textTheme.displayMedium!.copyWith(
+      color: theme.colorScheme.onPrimary,
+    );
+    return Card(
+      color: theme.colorScheme.primary,    // ← And also this.
+      child: Padding(
+        padding: const EdgeInsets.all(20),
+        child: Text(
+          pair.asLowerCase,
+          style: style,
+          semanticsLabel: "${pair.first} ${pair.second}"
+          ),
+      ),
+    );
+}
 }
